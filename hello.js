c98ea3dbd68eca370ee18ccad6a48f4851ab51ea
Marks Korņilovs <marks.kornilovs@visma.com>
2022-08-04 11:41:49 +0300

    new branch

--- hello.js
+++ hello.js
@@ -1,6 +1,11 @@
 console.log('Hello world');
+array.forEach(elemetn => {});
 
 // git init // new repo
 // git add * // add files
 // git commit -m "massage" // commit files
-// git log // sow commit history
+// git log // save commit history
+// git checkout -b 'name of the branch'  // create branch
+// git checkout master (vai izmanto branch)
+// git merge (branch name here)
+// git push
